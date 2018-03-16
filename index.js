57d5cd3ffd6bc2a0d9d444d890ec4e7932120f6d
Josephaberry54 <josephaberry54@gmail.com>
2018-03-16 17:39:29 +0000

    updated port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const app = require('./server');
-const PORT = require('./config').PORT[process.env.NODE_ENV];
+const app = require("./server");
+const PORT = require("./config").PORT[process.env.NODE_ENV] || process.env.PORT;
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log(`listening on port ${PORT}`);
 });
