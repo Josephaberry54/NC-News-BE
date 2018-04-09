eff9feddf197c78d4d945fd3f049c1a4de3348f6
Josephaberry54 <josephaberry54@gmail.com>
2018-04-09 10:16:13 +0100

    allowing put request (cors)

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ mongoose
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods: POST, GET, DELETE, PUT");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
